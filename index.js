b0c2c51ccb9e4fc765127f76d2f2d52fdcefdc2c
Jolyon <jolyon@conduce.co>
2018-05-06 12:32:50 +1200

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+
+exports.name = 'merge'
+exports.version = require('./package.json').version
+exports.manifest = {
+  get: 'async'
+}
+
+exports.init = function (ssb, config) {
+  return get
+
+  function get () {
+    console.log('ssb-merge')
+    return 'ssb-merge'
+  }
+
+}
